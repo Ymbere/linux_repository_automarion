403500ddd6d063ebf9da4114fe349af223c01bb9
Ymberê <ymbere@gmail.com>
2018-03-29 11:00:23 -0300

    Fixed bugs in script

--- script_1.py
+++ script_1.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env python
 # Encoding: utf-8
 
@@ -6,15 +8,15 @@ import io
 import stat
 import sys
 
-def touch(path)
-    with open(path, 'a')
+def touch(path):
+    with open(path, 'a'):
         os.utime(path, None)
 
 pkg_name = "isc-dhcp-server"
 
 os.system('apt-get install -y isc-dhcp-server')
 
-path_file = '/etc/dhcp/dhcpdf.conf'
+path_file = '/etc/dhcp/dhcpd.conf'
 
 with open(path_file, 'r') as file:
     data = file.readlines()
@@ -28,9 +30,9 @@ data[52] = 'subnet 10.5.5.0 netmask 255.255.255.224 {\n'
 
 data[53] = 'range 172.16.0.2 172.16.0.5;\n'
 
-data[56] = 'option subnet-mask 255.255.255.0;\n''
+data[56] = 'option subnet-mask 255.255.255.0;\n'
 
-data[58] = 'option broadcast-address 10.5.5.31;\n
+data[58] = 'option broadcast-address 10.5.5.31;\n'
 
 data[59] = 'default-lease-time 86400;\n'
 
