9e70b1b71adeb4dd1ba0be8a7d9a51a0d7f7ad6c
Ramos Ymbere <ymbere.ramos@thomsonreuters.com>
2018-04-21 01:19:35 -0300

    Improvement, need test

--- script_1.py
+++ script_1.py
@@ -1,6 +1,6 @@
 
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 # Encoding: utf-8
 
 import os
@@ -8,9 +8,23 @@ import io
 import stat
 import sys
 
+
 def touch(path):
     with open(path, 'a'):
         os.utime(path, None)
+
+
+def get_line_number(search, path):
+    with open(path) as myFile:
+        for num, line in enumerate(myFile, 0):
+            if search in line:
+                return num
+
+
+def write_file(file_edited, path):
+    with open(path, 'w') as f:
+        f.writelines(file_edited)
+
 
 os.system('apt-get install -y isc-dhcp-server')
 
@@ -19,53 +33,19 @@ path_file = '/etc/dhcp/dhcpd.conf'
 with open(path_file, 'r') as file:
     data = file.readlines()
 
+data[get_line_number('#authoritative;', path_file)] = 'authoritative;\n'
 
-data[23] = 'authoritative;\n'
+data[get_line_number('#subnet 10.5.5.0 netmask 255.255.255.224 {', path_file)] = \
+    'subnet 172.16.0.0 netmask 255.255.255.0 {\n'
 
-data[27] = 'log-facility local7;\n'
-
-data[52] = 'subnet 172.16.0.0 netmask 255.255.255.0 {\n'
-
-data[53] = ' range 172.16.0.2 172.16.0.50;\n'
+data[get_line_number('# range 10.5.5.26 10.5.5.30;'), path_file] = ' range 172.16.0.2 172.16.0.50;\n'
 
 data[56] = ' option subnet-mask 255.255.255.0;\n'
 
-data[58] = ' option broadcast-address 172.16.0.255;\n'
+data[get_line_number('# option broadcast-address 10.5.5.31;', path_file)] = ' option broadcast-address 172.16.0.255;\n'
 
-data[59] = ' default-lease-time 86400;\n'
+data[get_line_number('# default-lease-time 600;', path_file)] = ' default-lease-time 86400;\n'
 
-data[60] = ' max-lease-time 604800;\n'
+data[get_line_number('# max-lease-time 7200;', path_file)] = ' max-lease-time 604800;\n'
 
-data[61] = '}\n'
-
-with open(path_file, 'w') as file:
-    file.writelines(data)
-
-path_file_2 = '/etc/default/isc-dhcp-server'
-
-test = 'enp0s3'
-
-with open(path_file_2, 'r') as file:
-    data = file.readlines()
-
-data[16] = 'INTERFACES="enp0s3"\n'
-
-with open(path_file_2, 'w') as file:
-    file.writelines(data)
-
-path_file_3 = '/etc/network/interfaces'
-
-with open(path_file_3, 'r') as file:
-    data = file.readlines()
-
-data[11] = '#iface enp0s3 inet dhcp\n'
-
-new_lines = """iface enp0s3 inet static\n
-address 172.16.0.1\n
-network 172.16.0.0/24\n
-netmask 255.255.255.0\n
-broadcast 172.16.0.255\n"""
-
-with open(path_file_3, 'w') as file:
-    file.writelines(data)
-    file.writelines(new_lines)
+write_file(data, path_file)
