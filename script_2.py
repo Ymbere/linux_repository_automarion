f834b4fa77d01b090137441d4c361945b07c59c2
Ramos Ymbere <ymbere.ramos@thomsonreuters.com>
2018-04-21 18:23:57 -0300

    Update script to use system package

--- script_2.py
+++ script_2.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env/ python
-#Encodign: utf-8
+# !/usr/bin/env/ python
+# Encoding: utf-8
 
-import io
-import os
-import stat
-#52
+from system.system_operations import SystemOperations
 
-def touch(path):
-    with open(path, 'a'):
-        os.utime(path, None)
+s = SystemOperations
 
 path_file = '/etc/apt/sources.list'
-mode = stat.S_IREAD
 
-if os.path.exists(path_file):    
-    os.remove(path_file)
+s.remove_path_if_exists(path_file)
 
-touch(path_file)
+s.touch(path_file)
 
 with open(path_file, 'r') as file:
     data = file.readlines()
@@ -27,5 +20,4 @@ deb http://http.debian.net/debian/ jessie main contrib non-free\n
 deb http://http.debian.net/debian/ jessie-updates main contrib non-free\n
 deb http://http.debian.net/debian/ jessie-backports main contrib non-free"""
 
-with open(path_file, 'w') as file:
-    file.writelines(data)
+s.write_file(path_file, data)
